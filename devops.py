de7a57f1e75b7f94162b580f7490179996d44cfd
gbercsenyi-vm <gabor.bercsenyi@snt.hu>
2020-07-15 17:11:09 +0200

    Fixied bugs in devops.py

--- devops.py
+++ devops.py
@@ -1,9 +1,5 @@
 import json
-
 print("Hello World")
-
-json_text = '"key1": "value1", "key2": "value2"'
-
+json_text = '{"key1": "value1", "key2": "value2"}'
 json_dict = json.loads(json_text)
-
 print(json.dumps(json_dict, indent=2))
